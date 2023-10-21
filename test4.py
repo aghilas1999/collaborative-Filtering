e696001409a50ad4078cca8e09b1541781036cca
like a Boss <aghilas.smail@gmail.com>
2023-10-22 00:48:14 +0200

    test 4 fonctionne bien ca rest a confirmé

--- test4.py
+++ test4.py
@@ -1,80 +1,136 @@
-'''
-@article{10.1145/2827872,
-author = {Harper, F. Maxwell and Konstan, Joseph A.},
-title = {The MovieLens Datasets: History and Context},
-year = {2015},
-issue_date = {January 2016},
-publisher = {Association for Computing Machinery},
-address = {New York, NY, USA},
-volume = {5},
-number = {4},
-issn = {2160-6455},
-url = {https://doi.org/10.1145/2827872},
-doi = {10.1145/2827872},
-journal = {ACM Trans. Interact. Intell. Syst.},
-month = dec,
-articleno = {19},
-numpages = {19},
-keywords = {Datasets, recommendations, ratings, MovieLens}
-}
-'''
 
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
-movies_dataframe = pandas.read_csv("movies.csv")
-ratings_dataframe = pandas.read_csv("ratings.csv")
+# Chargez les données depuis les fichiers CSV
+movies_dataframe = pd.read_csv("movies.csv")
+ratings_dataframe = pd.read_csv("ratings.csv")
+
+def pearson_similarity(user_ratings1, user_ratings2):
+    # Rejoignez les évaluations des deux utilisateurs
+    merged_ratings = user_ratings1.join(user_ratings2, lsuffix="_user1", rsuffix="_user2").dropna()
+
+    if len(merged_ratings) == 0:
+        return 0  # Aucune évaluation commune, la similarité est nulle
+
+    # Calculez la moyenne des évaluations de chaque utilisateur
+    mean_user1 = user_ratings1['rating'].mean()
+    mean_user2 = user_ratings2['rating'].mean()
+
+    # Calculez la similarité de Pearson
+    numerator = ((merged_ratings['rating_user1'] - mean_user1) * (merged_ratings['rating_user2'] - mean_user2)).sum()
+    denominator_user1 = ((merged_ratings['rating_user1'] - mean_user1) ** 2).sum()
+    denominator_user2 = ((merged_ratings['rating_user2'] - mean_user2) ** 2).sum()
+    denominator = (denominator_user1 * denominator_user2) ** 0.5
+
+    if denominator == 0:
+        return 0  # Évitez la division par zéro
+
+    similarity = numerator / denominator
+    return similarity
 
 def find_user_distance(userId1, userId2):
-    user_1_ratings = find_user_ratings(userId1)
-    user_2_ratings = find_user_ratings(userId2)
-
-    ratings_comparison = user_1_ratings.join(user_2_ratings, lsuffix = "_user1", rsuffix = "_user2").dropna() #join user ratings
-
-    user_1_vector = ratings_comparison["rating_user1"].to_numpy()
-    user_2_vector = ratings_comparison["rating_user2"].to_numpy()
-    absolute_user_distance = numpy.linalg.norm(user_1_vector - user_2_vector) #calculate euclidean distance between user ratings
-
-    return [userId1, userId2, absolute_user_distance]
+    user_ratings1 = find_user_ratings(userId1)
+    user_ratings2 = find_user_ratings(userId2)
+    
+    similarity = pearson_similarity(user_ratings1, user_ratings2)
+    
+    return [userId1, userId2, similarity]
 
 def find_user_ratings(userID):
     user_ratings = ratings_dataframe.query(f"userId == {userID}")
-    return user_ratings[["movieId", "rating"]].set_index("movieId") #return user ratings
+    return user_ratings[["movieId", "rating"]].set_index("movieId")
 
 def find_relative_distance(userId):
-    users = ratings_dataframe["userId"].unique() #find all users
-    users = users[users != userId] #remove passed in user from list of users
-    distances = [find_user_distance(userId, every_other_user_id) for every_other_user_id in users] #find distances between user and all other users
-
-    return pandas.DataFrame(distances, columns=["masterUserId", "userId", "distance"]) #return dataframe of distances between our master user and all other users
+    users = ratings_dataframe["userId"].unique()
+    users = users[users != userId]
+    distances = [find_user_distance(userId, every_other_user_id) for every_other_user_id in users]
+    
+    return pd.DataFrame(distances, columns=["masterUserId", "userId", "similarity"])
 
 def find_closest_users(userId, number_of_users):
     relative_distances = find_relative_distance(userId)
-    relative_distances.sort_values("distance", inplace=True) #sort distances
-    return relative_distances.head(number_of_users) #return top n closest users
+    relative_distances.sort_values("similarity", ascending=False, inplace=True)
+    return relative_distances.head(number_of_users)
 
 def make_recommendation(userId):
     user_ratings = find_user_ratings(userId)
     closest_users = find_closest_users(userId, 10)
-    most_similar_user_id = closest_users.iloc[0]["userId"] #find most similar user
-
-    closest_user_ratings = find_user_ratings(most_similar_user_id) #find most similar user's ratings
-    unwatched_movies = closest_user_ratings.drop(user_ratings.index, errors= "ignore") #find movies the most similar user has watched that the user has not watched
-
-    unwatched_movies = unwatched_movies.sort_values("rating", ascending=False) #sort movies by rating with highest on top
+    most_similar_user_id = closest_users.iloc[0]["userId"]
+    
+    closest_user_ratings = find_user_ratings(most_similar_user_id)
+    unwatched_movies = closest_user_ratings.drop(user_ratings.index, errors="ignore")
+    
+    unwatched_movies = unwatched_movies.sort_values("rating", ascending=False)
     unwatched_movies = unwatched_movies.join(movies_dataframe)
 
-    return unwatched_movies.head(10) #return top 10 movies
+    return unwatched_movies.head(10)
 
-print(make_recommendation(9))
+# Exemple d'utilisation
+recommendations = make_recommendation(90)
+print(recommendations)
 
-movies_dataframe.set_index("movieId", inplace = True)
-total_rating_count = ratings_dataframe["movieId"].value_counts()
 
-movies_dataframe["totalRatingCount"] = total_rating_count   #add total rating count column to movies dataframe
 
-average_movie_ratings = ratings_dataframe.groupby("movieId").mean()["rating"]
-movies_dataframe["averageRating"] = average_movie_ratings #add average rating column to movies dataframe
+'''
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import cosine
 
-movies_dataframe.sort_values(["totalRatingCount", "averageRating"], ascending=False) #sort movies dataframe by total rating count
-min_ratings_subset = movies_dataframe.query(f"totalRatingCount >= {100}") #query movies dataframe for movies with over 100 ratings
+# Chargez les données depuis les fichiers CSV
+movies_dataframe = pd.read_csv("movies.csv")
+ratings_dataframe = pd.read_csv("ratings.csv")
+
+def cosine_similarity(user_ratings1, user_ratings2):
+    # Fusionnez les évaluations des deux utilisateurs
+    merged_ratings = user_ratings1.join(user_ratings2, lsuffix="_user1", rsuffix="_user2").dropna()
+
+    if len(merged_ratings) == 0:
+        return 0  # Aucune évaluation commune, la similarité est nulle
+
+    # Calculez la similarité du cosinus
+    similarity = 1 - cosine(merged_ratings['rating_user1'], merged_ratings['rating_user2'])
+    
+    return similarity
+
+def find_user_distance(userId1, userId2):
+    user_ratings1 = find_user_ratings(userId1)
+    user_ratings2 = find_user_ratings(userId2)
+    
+    similarity = cosine_similarity(user_ratings1, user_ratings2)
+    
+    return [userId1, userId2, similarity]
+
+def find_user_ratings(userID):
+    user_ratings = ratings_dataframe.query(f"userId == {userID}")
+    return user_ratings[["movieId", "rating"]].set_index("movieId")
+
+def find_relative_distance(userId):
+    users = ratings_dataframe["userId"].unique()
+    users = users[users != userId]
+    distances = [find_user_distance(userId, every_other_user_id) for every_other_user_id in users]
+    
+    return pd.DataFrame(distances, columns=["masterUserId", "userId", "similarity"])
+
+def find_closest_users(userId, number_of_users):
+    relative_distances = find_relative_distance(userId)
+    relative_distances.sort_values("similarity", ascending=False, inplace=True)
+    return relative_distances.head(number_of_users)
+
+def make_recommendation(userId):
+    user_ratings = find_user_ratings(userId)
+    closest_users = find_closest_users(userId, 500)
+    most_similar_user_id = closest_users.iloc[0]["userId"]
+    
+    closest_user_ratings = find_user_ratings(most_similar_user_id)
+    unwatched_movies = closest_user_ratings.drop(user_ratings.index, errors="ignore")
+    
+    unwatched_movies = unwatched_movies.sort_values("rating", ascending=False)
+    unwatched_movies = unwatched_movies.join(movies_dataframe)
+
+    return unwatched_movies.head(10)
+
+# Exemple d'utilisation
+recommendations = make_recommendation(90)
+print(recommendations)
+'''
